38af9bcd5d84aebcf07346951f8319bf48a826c8
Nick <demidov_nn@inbox.ru>
2021-01-07 17:15:45 +0300

    reveal secret_key

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,15 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#print("BASE_DIR:", BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
-with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\SECRET_KEY.txt") as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'w-@3s8u5vlqg$-k%-)3w3u9#t1f%z(^m%^y7j%*5n3focbs6u&'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
